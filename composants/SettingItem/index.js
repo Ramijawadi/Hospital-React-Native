33fa1b6e6ee3a74385fcf2fb5d20a06cc6deb858
rami jawadi <ramijawadi104@gmail.com>
2023-09-22 09:23:58 +0100

    fix all tasks : page doctors , conversations and settings

--- composants/SettingItem/index.js
+++ composants/SettingItem/index.js
@@ -1,13 +1,13 @@
 /* eslint-disable prettier/prettier */
-import { TouchableOpacity , Text } from 'react-native'
+import { TouchableOpacity, Text } from 'react-native'
 import React from 'react'
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import styles from './style';
-const SettingItem = ({item}) => {
+const SettingItem = ({ item }) => {
   return (
     <TouchableOpacity style={styles.container}>
       <Text>{item.libelle}</Text>
-      <AntDesign name='arrowright' size={20} color = {'lightgreen'} />
+      <AntDesign name='arrowright' size={20} color={'green'} />
     </TouchableOpacity>
   )
 }
